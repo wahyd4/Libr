afc2932086e2eb7a658c88cf61caa04e20e1cc97
Junv <wahyd4@gmail.com>
2013-01-08 12:29:33 +0800

    update book page

--- app/assets/javascripts/book.js
+++ app/assets/javascripts/book.js
@@ -1,1 +1,36 @@
+$(document).ready(function(){
+
+
+    loadBook = function(){
+        var isbn = $('input.isbn').val();
+        if(isbn === null){
+            return;
+        }
+        $.ajax({
+            url:'https://api.douban.com/v2/book/isbn/'+isbn+'?alt=xd',
+            dataType:"jsonp",
+            success:function(json){
+                console.log(json)
+                displayBookInfo(json);
+            }
+        });
+    }
+    displayBookInfo = function(json){
+        $('img.cover').attr('src',json.images.large);
+        var ul = $('div.span4 ul');
+        ul.append('<li><b>副标题:</b>'+json.subtitle+'</li>');
+        ul.append('<li><b>作者:</b>'+json.author[0]+'</li>');
+        ul.append('<li><b>出版社:</b>'+json.publisher+'</li>');
+        ul.append('<li><b>页数:</b>'+json.pages+'页</li>');
+        ul.append('<li><b>ISBN:</b>'+json.isbn13+'</li>');
+        ul.append('<li><b>定价:</b>'+json.price+'</li>');
+        $('div.well.summary').text(json.author_intro);
+
+    }
+
+    loadBook();
+
+
+
+});
 
--- spec/controllers/user_controller_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe UserControllerController do
-
-end
