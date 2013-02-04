15e3a397979f6f9cab6838bb00274e5628c3b3dc
Junv <wahyd4@gmail.com>
2013-02-04 11:29:05 +0800

    add gzip expires time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Libr::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.static_cache_control = "public, max-age=3600"
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
