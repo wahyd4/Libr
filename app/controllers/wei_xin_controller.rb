08a48f9a96882375f437678068a4bc17d0dc85e2
Junv <wahyd4@gmail.com>
2013-04-17 22:51:39 +0800

    add return mix message for weixin

--- app/controllers/wei_xin_controller.rb
+++ app/controllers/wei_xin_controller.rb
@@ -8,14 +8,39 @@ class WeiXinController < ApplicationController
   end
 
   def query
-    xml_string = request.raw_post
     message = parse_xml_to_hash(params[:xml])
-    reply_message = BasicMessage.new
-    reply_message.create_time = Time.now.to_i
-    reply_message.from_user = message.to_user
-    reply_message.to_user = message.from_user
-    reply_message.message_type = message.message_type
-    reply_message.message_id=''
+    case message.content
+      when '1'
+        books = Book.order('id DESC').limit(5)
+        reply_message = build_reply(message)
+        builder = Nokogiri::XML::Builder.new do |xml|
+          xml.xml {
+            xml.FromUserName reply_message.from_user
+            xml.ToUserName reply_message.to_user
+            xml.CreateTime reply_message.create_time
+            xml.MsgType 'news'
+            xml.ArticleCount books.count
+            xml.Articles {
+              books.each do |book|
+                xml.item {
+                  xml.Title book.name
+                  xml.Description book.author
+                  xml.PicUrl book.image
+                }
+              end
+            }
+          }
+        end
+        result = builder.to_xml
+      else
+        result = reply_text_message(message)
+    end
+
+    render xml: result
+  end
+
+  def reply_text_message(message)
+    reply_message = build_reply(message)
     reply_message.content = "Hello Thoughtworks"
 
     builder = Nokogiri::XML::Builder.new do |xml|
@@ -28,8 +53,16 @@ class WeiXinController < ApplicationController
         xml.FuncFlag 0
       }
     end
+    builder.to_xml
+  end
 
-    render xml: builder.to_xml
+  def build_reply(message)
+    reply_message = BasicMessage.new
+    reply_message.create_time = Time.now.to_i
+    reply_message.from_user = message.to_user
+    reply_message.to_user = message.from_user
+    reply_message.message_type = message.message_type
+    reply_message
   end
 
   def parse_xml(xml_string)
