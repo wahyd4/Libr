89d178edbee0dbaa3090d7540ca0fc72d59b9900
junv <wahyd4@gmail.com>
2014-01-20 15:08:23 +0800

    replace sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140117135051) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "auth_keys", force: true do |t|
     t.integer  "user_id"
     t.string   "name"
@@ -86,7 +89,7 @@ ActiveRecord::Schema.define(version: 20140117135051) do
     t.string   "last_sign_in_ip"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
