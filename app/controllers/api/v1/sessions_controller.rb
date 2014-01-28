ae14989f4987c81aba5645b9045df1e9a32caa16
junv <wahyd4@gmail.com>
2014-01-28 12:44:52 +0800

    add skip CSRF token when call login from API

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::SessionsController < Devise::SessionsController
 
+
   before_filter :allow_cors
+  #skip_before_filter :verify_authenticity_token
 
   def create
     warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
