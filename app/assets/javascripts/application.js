262a3f08b9a66ed45d7d9d30a2fe43e067da80e7
junv <wahyd4@gmail.com>
2014-04-14 17:09:49 +0800

    add migration guide to website,and remove android app temporally

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,11 @@
 //= require jquery
 //= require jquery_ujs
+
+$(document).ready(function () {
+    if (window.location.href.lastIndexOf('libr.herokuapp.com') !== -1) {
+
+        if (confirm('此网站已经迁移至: http://librme.com,点击确定跳转到该网站')) {
+            window.location.href = 'http://librme.com'
+        }
+    }
+});
